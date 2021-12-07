52504cf92b0bec155f04300251ec06cba91729ca
shivanagouda <shivanagouda.g@mobiotics.com>
2021-12-07 23:47:29 +0530

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const PORT = process.env.PORT || 3000;
 
 app.get("", (req,res) => {
-    res.send("Hello word");
+    res.send("Hello word1");
 });
 app.listen(PORT, () => {
     console.log(`APP up at port ${PORT}`);
